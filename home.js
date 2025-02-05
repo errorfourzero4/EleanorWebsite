9c7acacc448550aea1a8073c4b3e7ce8e083408f
errorfourzero4 <ng.chiewgwun@gmail.com>
2025-02-05 12:46:04 +0000

    Finished left flower stem drawing

--- home.js
+++ home.js
@@ -9,24 +9,24 @@ const HEIGHT = canvas.height;
 let flower = {x: 234, y: HEIGHT, dx: 5, color: "rgb(16, 141, 43)"};
 
 // Functions for drawing left flower stem
-function updateFlowers() {
+function updateStem() {
   flower.x += flower.dx;
   flower.y = (0.1 * ((flower.x - 300)**2)) - flower.x + 700;
 }
 
-function startFlowers() { 
-  if(flower.x < 310){ requestAnimationFrame(startFlowers); }
+function startStem() { 
+  if(flower.x < 310){ requestAnimationFrame(startStem); }
 
   context.strokeStyle = flower.color;
   context.lineWidth = 5;
   context.beginPath();
   console.log("1: " + flower.y);
   context.moveTo(flower.x, flower.y);
-  updateFlowers();
+  updateStem();
   console.log("2: " + flower.y);
   context.lineTo(flower.x, flower.y);
   context.stroke();
 }
 
 // Code
-startFlowers();
+startStem();
