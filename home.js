24b528753b717a7392d0320a29630193ff752d6d
errorfourzero4 <ng.chiewgwun@gmail.com>
2025-02-05 20:38:06 +0000

    Left flower fully created

--- home.js
+++ home.js
@@ -16,13 +16,19 @@ function startAnimating() {
 }
 
 // Functions for drawing left flower stem
-function updateStem() {
+function updateLeftStem() {
   flower.x += flower.dx;
   flower.y = (0.1 * ((flower.x - 300)**2)) - flower.x + 700;
 }
 
-function startStem() { 
-  if(flower.x < 310){ requestAnimationFrame(startStem); }
+function startLeftStem() { 
+  if(flower.x < 310){ 
+    requestAnimationFrame(startLeftStem); 
+  }
+  else {
+    setup();
+    loop();
+  }
 
   now = Date.now();
   elapsed = now - then;
@@ -34,15 +40,71 @@ function startStem() {
     context.strokeStyle = flower.color;
     context.lineWidth = 5;
     context.beginPath();
-    console.log("1: " + flower.y);
     context.moveTo(flower.x, flower.y);
-    updateStem();
-    console.log("2: " + flower.y);
+    updateLeftStem();
     context.lineTo(flower.x, flower.y);
     context.stroke();
   }
 }
 
+function updateRightStem() {
+  // code
+}
+
+function startRightStem() {
+  // code
+}
+
 // Code
 setTimeout(function() { startAnimating(); }, 1000);
-startStem();
+startLeftStem();
+
+
+
+// Rose
+
+// Rose (mathematics) function.
+// https://en.wikipedia.org/wiki/Rose_(mathematics)
+//
+// What is k, n, d variable?
+// you can understand to see follow svg image.
+// https://en.wikipedia.org/wiki/Rose_(mathematics)#/media/File:Rose-rhodonea-curve-7x9-chart-improved.svg
+function rose(theta, n, d, amplitude) {
+  var k = n / d;
+  var x = amplitude * Math.cos(k * theta) * Math.cos(theta);
+  var y = amplitude * Math.cos(k * theta) * Math.sin(theta);  
+  return {"x":x, "y": y};
+}
+
+function point(x, y, context) {
+    context.beginPath();
+    context.arc(x, y, 1, 0, 2 * Math.PI, true);
+    context.stroke();
+}
+
+var nodes;
+var t = 0;
+
+function setup() {
+  // bloom.
+  context.shadowBlur = 10;
+  context.shadowColor = 'rgb(0, 0, 0)';
+  
+  // point style
+  context.strokeStyle = 'rgb(192, 23, 207)';
+}
+
+function loop() {
+  window.requestAnimationFrame(loop);
+
+  context.translate(WIDTH/2, HEIGHT/2); // (0, 0) set to screen center position.
+
+  var p = rose(t, 3, 2, 100.0);
+  point(p.x, p.y - 50, context);
+
+
+  
+  context.translate(-WIDTH/2, -HEIGHT/2); // reset screen
+  
+  t += 0.05;
+}
